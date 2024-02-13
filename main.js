6b328aa00d6935f31869f69f5e6f485c97ae67ff
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-02-14 02:26:15 +0500

    Initial Commit

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
+import { ethers } from "ethers";
 import "./style.css";
-import javascriptLogo from "/javascript.svg";
-import viteLogo from "/vite.svg";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-  </div>
-`;
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+console.log(provider);
